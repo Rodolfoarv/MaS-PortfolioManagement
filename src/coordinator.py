1c4603bc29de51e879852f4ffbd30e8880f7cd9c
Rodolfoarv <rodolfo@ramirezvalenzuela.com>
2016-10-09 22:03:04 -0500

    Refactor for multiple queries

--- src/coordinator.py
+++ src/coordinator.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 '''
 Coordinator Agent description, send messages to their agents
 '''
@@ -13,52 +14,47 @@ host = "127.0.0.1"
 class Coordinator(spade.Agent.Agent):
 
     def _setup(self):
-        #Set the template for QueryAllShares
-        templateQ1 = spade.Behaviour.ACLTemplate()
+        # Set the template for the Queries
+        template = spade.Behaviour.ACLTemplate()
         template.setOntology("MaS")
         template.setPerformative("request")
         template.setConversationId("q1")
         mt = spade.Behaviour.MessageTemplate(template)
 
-        #Add Behaviour for Query 1 - Query all share’s quotation on the current day
-		self.addBehaviour(self.QueryAllShares(), mt)
+        # Add Behaviour for Queries
+    	self.addBehaviour(self.TechnicalAnalysis(), mt)
+    	print "Sender started!"
 
-        #Add Behaviour for Query 2 - Query a given share’s quotation on the current day
-        template.setPerformative("request")
-        template.setConversationId("q2")
+    class TechnicalAnalysis(spade.Behaviour.OneShotBehaviour):
 
-        self.addBehaviour(self.QueryGivenShare())
-		print "Sender started!"
-
-    class QueryAllShares(spade.Behaviour.OneShotBehaviour):
-
-        def _process(self):
+        #Query all share’s quotation on the current day
+        def q1(self):
+            print "test"
             msg = spade.ACLMessage.ACLMessage()
             msg.setPerformative("request")
             msg.addReceiver(spade.AID.aid("technical_analysis@"+host,["xmpp://technical_analysis@"+host]))
             msg.setContent("SHARE ALL")
             self.myAgent.send(msg)
 
-            print "coordinator has sent coordinator message:"
-            print str(msg)
-
-    class QueryGivenShare(spade.Behaviour.OneShotBehaviour):
-
-        def _process(self):
+        #Query all share’s quotation on the current day
+        def q2(self):
+            print "test"
             msg = spade.ACLMessage.ACLMessage()
             msg.setPerformative("request")
             msg.addReceiver(spade.AID.aid("technical_analysis@"+host,["xmpp://technical_analysis@"+host]))
-            msg.setContent("SHARE ONE")
-
+            msg.setContent("SHARE ALL")
             self.myAgent.send(msg)
 
-            print "coordinator has sent coordinator message:"
-            print str(msg)
+        def _process(self):
+            self.q1()
+            #Wait until the technical_analysis responds to us
+            self.msg = self.receive(True)
 
 
 if __name__ == "__main__":
 	coordinator = Coordinator("coordinator@"+host,"secret")
 	coordinator.start()
+
         alive = True
         while alive:
             try:
