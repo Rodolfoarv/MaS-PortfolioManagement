f64ec1c4ac394c8c1ebd8360a85ca56f0f6f4213
Rodolfoarv <rodolfo@ramirezvalenzuela.com>
2016-10-30 17:39:29 -0600

    Modification to the communication protocol between coordinator and technical analysis

--- src/coordinator.py
+++ src/coordinator.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+    # -*- coding: utf-8 -*-
 '''
 Coordinator Agent description, send messages to their agents
 '''
@@ -14,46 +14,36 @@ HOST = "127.0.0.1"
 class Coordinator(spade.Agent.Agent):
 
     def _setup(self):
-        # Set the template for the Queries
         template = spade.Behaviour.ACLTemplate()
         template.setOntology("MaS")
-        template.setPerformative("request")
-        template.setConversationId("q1")
+        template.setPerformative("inform")
+        template.setConversationId("TechnicalAnalysis")
         mt = spade.Behaviour.MessageTemplate(template)
+        self.addBehaviour(self.TechnicalAnalysisBehav(),mt)
 
-        # Add Behaviour for Queries
-    	self.addBehaviour(self.TechnicalAnalysis(), mt)
-    	print "Sender started!"
+    class TechnicalAnalysisBehav(spade.Behaviour.OneShotBehaviour):
 
-        #Add Behaviour for results
-        template.setConversationId("result")
-        mt = spade.Behaviour.MessageTemplate(template)
-        self.addBehaviour(self.Result(),mt)
-
-
-
-    class TechnicalAnalysis(spade.Behaviour.OneShotBehaviour):
-
-        def onEnd(self):
-            print "Closing agent.."
-            self.myAgent._kill()
+        def _process(self):
+            self.q1()
+            print "Waiting for response"
+            self.msg = self._receive(True)
+            print "Coordinator agent has received the response"
+            print str(msg.getContent())
 
         #Query all share’s quotation on the current day
         def q1(self):
             msg = spade.ACLMessage.ACLMessage()
-            msg.setPerformative("request")
-            msg.addReceiver(spade.AID.aid("technical_analysis@"+HOST,["xmpp://technical_analysis@"+HOST]))
+            msg.setOntology("MaS")
+            msg.setPerformative("inform")
+            msg.setConversationId("TechnicalAnalysis")
             msg.setContent("Apple, Alphabet, IBM, Microsoft")
+            msg.addReceiver(spade.AID.aid("technical_analysis@"+HOST,["xmpp://technical_analysis@"+HOST]))
             self.myAgent.send(msg)
             print msg
 
         #Query all share’s quotation on the current day
         def q2(self):
-            msg = spade.ACLMessage.ACLMessage()
-            msg.setPerformative("request")
-            msg.addReceiver(spade.AID.aid("technical_analysis@"+HOST,["xmpp://technical_analysis@"+HOST]))
-            msg.setContent("SHARE ALL")
-            self.myAgent.send(msg)
+            pass
 
         # Query a given share’s real-time trading chart
         def q3(self):
@@ -75,22 +65,9 @@ class Coordinator(spade.Agent.Agent):
         def q7(self):
             pass
 
-        def _process(self):
-            self.q1()
-
-    class Result(spade.Behaviour.OneShotBehaviour):
-        def _process(self):
-            #Wait until the technical_analysis responds to us
-            print "Waiting for response"
-            self.msg = self._receive(True,10)
-            print self.msg
-
-
-
 if __name__ == "__main__":
 	coordinator = Coordinator("coordinator@"+HOST,"secret")
 	coordinator.start()
-
         alive = True
         while alive:
             try:
--- src/technical_analysis.py
+++ src/technical_analysis.py
@@ -10,51 +10,43 @@ from queries import query1
 HOST = "127.0.0.1"
 
 class TechnicalAnalysis(spade.Agent.Agent):
+# template.setSender(spade.AID.aid("coordinator@"+HOST,["xmpp://coordinator@"+HOST]))
     def _setup(self):
         template = spade.Behaviour.ACLTemplate()
-        template.setSender(spade.AID.aid("coordinator@"+HOST,["xmpp://coordinator@"+HOST]))
-        t = spade.Behaviour.MessageTemplate(template)
-        self.addBehaviour(self.RecvMsgBehav(),t)
-        print "TechnicalAnalysis started!"
+        template.setOntology("MaS")
+        template.setPerformative("inform")
+        template.setConversationId("TechnicalAnalysis")
+        mt = spade.Behaviour.MessageTemplate(template)
+        self.addBehaviour(self.RecvMsgBehav(),mt)
 
+    def sendToCoordinator(self, perf, id, content):
+     # Create the message Object
+     msg = spade.ACLMessage.ACLMessage()
+     # Fill it
+     msg.setOntology( "MaS" )
+     msg.setPerformative( perf )
+     msg.setConversationId( id )
+     msg.setContent( '[{"Empresa": "Microsoft", "PrecioApertura": 114.31, "PrecioClausura": 114.06, "Pico": 114.56, "Depresion": 113.51, "Volumen": 24329900}, {"Empresa": "Apple", "PrecioApertura": 113.7, "PrecioClausura": 113.89, "Pico": 114.34, "Depresion": 113.13, "Volumen": 28779300}, {"Empresa": "Alphabet", "PrecioApertura": 113.4, "PrecioClausura": 113.05, "Pico": 113.66, "Depresion": 112.69, "Volumen": 21453100}, {"Empresa": "IBM", "PrecioApertura": 113.06, "PrecioClausura": 113.0, "Pico": 114.31, "Depresion": 112.63, "Volumen": 29736800}]' )
 
+     receiver = spade.AID.aid(name="coordinator@127.0.0.1",addresses=["xmpp://coordinator@127.0.0.1"])
+     msg.addReceiver( receiver )
+     print msg
+     self._send(msg)
 
     class RecvMsgBehav(spade.Behaviour.OneShotBehaviour):
-
-        def sendToAgent(self, perf, id, content):
-         # Creamos el mensaje
-         msg = spade.ACLMessage.ACLMessage()
-         # Lo rellenamos
-         msg.setOntology( "MaS" )
-         msg.setPerformative( perf )
-         msg.setConversationId( id )
-         msg.setContent( '[{"Empresa": "Microsoft", "PrecioApertura": 114.31, "PrecioClausura": 114.06, "Pico": 114.56, "Depresion": 113.51, "Volumen": 24329900}, {"Empresa": "Apple", "PrecioApertura": 113.7, "PrecioClausura": 113.89, "Pico": 114.34, "Depresion": 113.13, "Volumen": 28779300}, {"Empresa": "Alphabet", "PrecioApertura": 113.4, "PrecioClausura": 113.05, "Pico": 113.66, "Depresion": 112.69, "Volumen": 21453100}, {"Empresa": "IBM", "PrecioApertura": 113.06, "PrecioClausura": 113.0, "Pico": 114.31, "Depresion": 112.63, "Volumen": 29736800}]' )
-
-         receiver = spade.AID.aid(name="coordinator@"+HOST,
-                                   addresses=["xmpp://coordinator@"+HOST])
-         msg.addReceiver( receiver )
-         print msg
-         self._sendTo(msg, "coordinator@"+HOST)
-
-        def onEnd(self):
-            print "Closing agent.."
-            self.myAgent._kill()
 
         def q1(self):
             print "Sleeping"
             time.sleep(5)
-            self.sendToAgent("request", "result", "10,20,30,40")
-            print "sent"
-
-
-
-        def q2(self):
-            return ""
+            print "Message has been sent: "
+            self.myAgent.sendToCoordinator("inform", "TechnicalAnalysis", None)
 
         def _process(self):
-            msg = self._receive(True)
+            print "Waiting for message"
+            self.msg = self._receive(True)
             print "technical_analysis has received a message:"
-            print str(msg.getContent())
+            print str(self.msg.getContent())
+            print "Sending back the results to the Coordinator agent"
             self.q1()
 
 
