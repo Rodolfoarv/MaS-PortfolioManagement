db147345a744c37e880a7a1ffdb33bb12bf991d3
Rodolfoarv <rodolfo@ramirezvalenzuela.com>
2016-10-30 18:21:00 -0600

    ADDED Technical Analysis documentation

--- src/technical_analysis.py
+++ src/technical_analysis.py
@@ -10,16 +10,24 @@ from queries import query1
 HOST = "127.0.0.1"
 
 class TechnicalAnalysis(spade.Agent.Agent):
-# template.setSender(spade.AID.aid("coordinator@"+HOST,["xmpp://coordinator@"+HOST]))
+    '''The technical analysis agent (TAA) retrieves and processes
+    the raw stock trading data from the Internet, stores the raw
+    data to a relevant database (HTDB, RTDB), calculates various
+    technical indicators, identifies various price and trading
+    volume patterns, and gives the output to the DMA.  '''
+
     def _setup(self):
+        '''Initial setup for the technical analysis, it will query what the coordinator asks for '''
         template = spade.Behaviour.ACLTemplate()
         template.setOntology("MaS")
-        template.setPerformative("inform")
+        template.setPerformative("request")
         template.setConversationId("TechnicalAnalysis")
         mt = spade.Behaviour.MessageTemplate(template)
         self.addBehaviour(self.RecvMsgBehav(),mt)
 
     def sendToCoordinator(self, performative, conversationID, content):
+        '''Function that takes as parameter the perfomative, conversation and content to
+        be sent back to the coordinator '''
         msg = spade.ACLMessage.ACLMessage()
         msg.setOntology("MaS")
         msg.setPerformative(performative)
@@ -30,7 +38,7 @@ class TechnicalAnalysis(spade.Agent.Agent):
         self.send(msg)
 
     class RecvMsgBehav(spade.Behaviour.OneShotBehaviour):
-
+        '''Class that will make the queries depending on the coordinator's request '''
         def _process(self):
             print "Waiting for message"
             self.msg = self._receive(True)
@@ -41,7 +49,7 @@ class TechnicalAnalysis(spade.Agent.Agent):
 
         def q1(self):
             content = '[{"Empresa": "Microsoft", "PrecioApertura": 114.31, "PrecioClausura": 114.06, "Pico": 114.56, "Depresion": 113.51, "Volumen": 24329900}, {"Empresa": "Apple", "PrecioApertura": 113.7, "PrecioClausura": 113.89, "Pico": 114.34, "Depresion": 113.13, "Volumen": 28779300}, {"Empresa": "Alphabet", "PrecioApertura": 113.4, "PrecioClausura": 113.05, "Pico": 113.66, "Depresion": 112.69, "Volumen": 21453100}, {"Empresa": "IBM", "PrecioApertura": 113.06, "PrecioClausura": 113.0, "Pico": 114.31, "Depresion": 112.63, "Volumen": 29736800}]'
-            self.myAgent.sendToCoordinator("inform", "TechnicalAnalysis", content )
+            self.myAgent.sendToCoordinator("request", "TechnicalAnalysis", content )
 
 
 
