9a32a260733898d0c511bdc846105528c77ba9c1
Rodolfoarv <rodolfo@ramirezvalenzuela.com>
2016-10-30 18:00:07 -0600

    REFACTOR: Query 1 functionality to sendToCoordinator method

--- src/technical_analysis.py
+++ src/technical_analysis.py
@@ -19,15 +19,15 @@ class TechnicalAnalysis(spade.Agent.Agent):
         mt = spade.Behaviour.MessageTemplate(template)
         self.addBehaviour(self.RecvMsgBehav(),mt)
 
-    def sendToCoordinator(self, perf, id, content):
+    def sendToCoordinator(self, performative, conversationID, content):
         msg = spade.ACLMessage.ACLMessage()
         msg.setOntology("MaS")
-        msg.setPerformative("inform")
-        msg.setConversationId("TechnicalAnalysis")
-        msg.setContent( '[{"Empresa": "Microsoft", "PrecioApertura": 114.31, "PrecioClausura": 114.06, "Pico": 114.56, "Depresion": 113.51, "Volumen": 24329900}, {"Empresa": "Apple", "PrecioApertura": 113.7, "PrecioClausura": 113.89, "Pico": 114.34, "Depresion": 113.13, "Volumen": 28779300}, {"Empresa": "Alphabet", "PrecioApertura": 113.4, "PrecioClausura": 113.05, "Pico": 113.66, "Depresion": 112.69, "Volumen": 21453100}, {"Empresa": "IBM", "PrecioApertura": 113.06, "PrecioClausura": 113.0, "Pico": 114.31, "Depresion": 112.63, "Volumen": 29736800}]' )
+        msg.setPerformative(performative)
+        msg.setConversationId(conversationID)
+        msg.setContent(content)
         msg.addReceiver(spade.AID.aid("coordinator@"+HOST,["xmpp://coordinator@"+HOST]))
-        self.myAgent.send(msg)
-        print msg
+        print "Sending....."
+        self.send(msg)
 
     class RecvMsgBehav(spade.Behaviour.OneShotBehaviour):
 
@@ -39,16 +39,10 @@ class TechnicalAnalysis(spade.Agent.Agent):
             print "Sending back the results to the Coordinator agent"
             self.q1()
 
-
         def q1(self):
-            msg = spade.ACLMessage.ACLMessage()
-            msg.setOntology("MaS")
-            msg.setPerformative("inform")
-            msg.setConversationId("TechnicalAnalysis")
-            msg.setContent( '[{"Empresa": "Microsoft", "PrecioApertura": 114.31, "PrecioClausura": 114.06, "Pico": 114.56, "Depresion": 113.51, "Volumen": 24329900}, {"Empresa": "Apple", "PrecioApertura": 113.7, "PrecioClausura": 113.89, "Pico": 114.34, "Depresion": 113.13, "Volumen": 28779300}, {"Empresa": "Alphabet", "PrecioApertura": 113.4, "PrecioClausura": 113.05, "Pico": 113.66, "Depresion": 112.69, "Volumen": 21453100}, {"Empresa": "IBM", "PrecioApertura": 113.06, "PrecioClausura": 113.0, "Pico": 114.31, "Depresion": 112.63, "Volumen": 29736800}]' )
-            msg.addReceiver(spade.AID.aid("coordinator@"+HOST,["xmpp://coordinator@"+HOST]))
-            print "Sending....."
-            self.myAgent.send(msg)
+            content = '[{"Empresa": "Microsoft", "PrecioApertura": 114.31, "PrecioClausura": 114.06, "Pico": 114.56, "Depresion": 113.51, "Volumen": 24329900}, {"Empresa": "Apple", "PrecioApertura": 113.7, "PrecioClausura": 113.89, "Pico": 114.34, "Depresion": 113.13, "Volumen": 28779300}, {"Empresa": "Alphabet", "PrecioApertura": 113.4, "PrecioClausura": 113.05, "Pico": 113.66, "Depresion": 112.69, "Volumen": 21453100}, {"Empresa": "IBM", "PrecioApertura": 113.06, "PrecioClausura": 113.0, "Pico": 114.31, "Depresion": 112.63, "Volumen": 29736800}]'
+            self.myAgent.sendToCoordinator("inform", "TechnicalAnalysis", content )
+
 
 
 if __name__ == "__main__":
