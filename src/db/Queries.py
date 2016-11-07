19ec10b341462f00059ea3a8648f1e1c1134bf96
Robie93 <torrez_cinq@hotmail.com>
2016-11-06 18:50:47 -0600

    Update Queries.py

--- src/db/Queries.py
+++ src/db/Queries.py
@@ -178,3 +178,45 @@ def q04(usuario):
 	conn.close()
 	#Muestra lo obtenido.
 	return jsonResult
+
+#Q05: Regresa todas las acciones de la empresa indicada dentro de un rango de fechas.
+def q05(empresa, inicio, final):
+	#Conecta a la base de datos.
+	conn = MySQLdb.Connect(host = db_host, port = db_port, user = db_usuario, passwd = db_password, db = base_de_datos)
+	#Crea un cursor.
+	cursor = conn.cursor()
+	#Query 04: Regresa todas las acciones de la empresa indicada dentro de un rango de fechas.
+	#En este caso recupera las de Apple, pero hay que cambiar ese campo cada vez que se quieran buscar acciones de cierta empresa.
+	query = "SELECT E.Nombre, A.PrecioApertura, A.PrecioClausura, A.Pico, A.Depresion, A.Volumen, A.Volatilidad FROM Accion AS A INNER JOIN Empresa AS E ON A.ID_Empresa=E.ID_EMPRESA WHERE fecha BETWEEN '%s' AND '%s' AND E.Nombre = '%s'" %(inicio, final, empresa)
+	#Se ejecuta el query disenado.
+	cursor.execute(query)
+
+	#Crea una lista de objetos.
+	objects_list = []
+	if query.upper().startswith('SELECT'): 
+		rows = cursor.fetchall()   # Lectura de datos.
+		#Crea un diccionario por cada registro devuelto del query.
+		for row in rows:
+			d = collections.OrderedDict()
+			d['Empresa'] = row[0]
+			d['PrecioApertura'] = float(row[1])
+			d['PrecioClausura'] = float(row[2])
+			d['Pico'] = float(row[3])
+			d['Depresion'] = float(row[4])
+			d['Volumen'] = int(row[5])
+			d['Volatilidad'] = int(row[6])
+			objects_list.append(d)
+		#Convierte a JSON los diccionarios.
+		jsonResult = json.dumps(objects_list)
+
+	else: 
+		#Escritura, modificacion o eliminacion de datos.
+		conn.commit()
+		rows = None 
+
+	#Cierra el cursor. 
+	cursor.close() 
+	#Cierra la conexion.                
+	conn.close()                   
+	#Muestra lo obtenido.
+	return jsonResult
